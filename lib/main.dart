68957d8ea87fb53e0f52c2f44106065a7dab08a0
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-05-14 14:30:02 -0400

    Create simple flash card that shows one problem
    The speech-to-text system breaks down what it hears into pieces;
    for example, saying 'sixty-three' will pick up '60' before it
    then picks up '63'. The current implementation has no handling for that.
    This is just a simple proof of concept.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:speech_to_text/speech_recognition_error.dart';
 
@@ -24,61 +25,47 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FlashCard(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class FlashCard extends StatefulWidget {
+  FlashCard({Key key, this.speech}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final SpeechToText speech;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _FlashCardState createState() => _FlashCardState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  bool _hasSpeech = false;
-  final SpeechToText speech = SpeechToText();
+enum FlashCardResult { waiting, correct, incorrect }
+
+class _FlashCardState extends State<FlashCard> {
+  final SpeechToText speech = new SpeechToText();
+  bool _hasSpeech;
+  FlashCardResult _result = FlashCardResult.waiting;
+  String _guess = "";
 
   @override
   void initState() {
     super.initState();
-    initSpeechState();
+    initSpeech();
   }
 
-  Future<void> initSpeechState() async {
-    print("Trying to initialize speech state");
+  void initSpeech() async {
     var hasSpeech = await speech.initialize(
         onError: errorListener,
         onStatus: statusListener,
         debugLogging: true,
         finalTimeout: Duration(milliseconds: 0));
 
-    // if (hasSpeech) {
-    //   _localeNames = await speech.locales();
-
-    //   var systemLocale = await speech.systemLocale();
-    //   _currentLocaleId = systemLocale.localeId;
-    // }
-
-    //if (!mounted) return;
-
     setState(() {
       _hasSpeech = hasSpeech;
       print("Has speech? " + (_hasSpeech ? "Yes" : "No"));
     });
+
+    speech.listen(onResult: onSpeechResult, listenFor: Duration(seconds: 2));
   }
 
   void errorListener(SpeechRecognitionError error) {
@@ -90,77 +77,19 @@ class _MyHomePageState extends State<MyHomePage> {
         'Received listener status: $status, listening: ${speech.isListening}');
   }
 
-  void _incrementCounter() {
-    speech.listen(
-        onResult: (result) {
-          print('Partial: ${result.recognizedWords}');
-          print('Final: ${result.finalResult}');
-        },
-        listenFor: Duration(seconds: 2),
-        pauseFor: Duration(seconds: 2),
-        partialResults: true,
-        cancelOnError: true,
-        listenMode: ListenMode.dictation);
-
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Column(
+        children: [Text("5*2"), Text(_guess), Text(_result.toString())]);
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      int spokenNumber = int.tryParse(result.recognizedWords);
+      _guess = (spokenNumber == null) ? "" : result.recognizedWords;
+      _result = result.recognizedWords.contains("10")
+          ? FlashCardResult.correct
+          : FlashCardResult.incorrect;
+    });
   }
 }
